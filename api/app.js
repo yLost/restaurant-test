73260d8327d704e5a8ad1675a37778a05987468d
Fernando <fernandopovinsky@hotmail.com>
2021-09-10 11:52:56 -0300

    Fix: body parser issue; Feat: Added options request method

--- api/app.js
+++ api/app.js
@@ -4,12 +4,17 @@ const bodyParser = require("body-parser");
 const morgan = require("morgan");
 
 app.use(morgan("dev"));
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json({ limit: '10mb' }));
 
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Authorization, Content-Type, X-Requested-With, Cookie");
+    res.header("Access-Control-Allow-Methods", "OPTIONS, POST, GET, PATCH");
+
+    if (req.method == "OPTIONS") {
+        return res.status(204).header("Allow", "OPTIONS, POST, GET, PATCH").send();
+    }
 
     next();
 });
